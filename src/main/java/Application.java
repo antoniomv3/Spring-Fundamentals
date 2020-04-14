4a64822d3df46761044d70e76fc09969e5f047bf
Antonio Vazquez <avazquez@shelterinsurance.com>
2020-04-14 09:22:32 -0500

    Setter Injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,14 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        //Application is hard coded, not great :(
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,17 +1,20 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    //Another hard coded reference with "new".
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
