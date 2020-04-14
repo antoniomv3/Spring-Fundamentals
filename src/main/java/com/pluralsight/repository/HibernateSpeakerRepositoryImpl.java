73c4910bf46477015fbed58c7706dcd7684ffad6
Antonio Vazquez <avazquez@shelterinsurance.com>
2020-04-14 10:10:50 -0500

    FactoryBeans

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,14 +1,21 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import com.pluralsight.util.CalendarFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     //This is a placeholder for database information. This could be a stub instead with spring loading in values instead
+
+    @Autowired
+    private Calendar cal;
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<Speaker>();
@@ -17,6 +24,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setFirstName("Antonio");
         speaker.setLastName("Vazquez");
 
+        System.out.println("cal: " + cal.getTime());
         speakers.add(speaker);
 
         return speakers;
